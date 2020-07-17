0a24ac8d65a742cd63d43e0dbd2a31e6f1f210d4
tindoann <tindoann@gamil.com>
2020-07-17 11:12:15 -0400

    added logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'; 
-// import './css/App.css'; 
+import React, { Component } from 'react';
+import Header from '../component/Header';  
+import '../css/App.css'; 
 
 class Main extends Component {
   render() {
     return (
       <div>
-        <h1>The Main Component</h1>
+        <Header />
       </div>
     )
   }
