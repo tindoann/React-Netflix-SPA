e13f22417cfa2e5da919a169e57ae203dfc11a3e
tindoann <tindoann@gamil.com>
2020-07-17 22:36:35 -0400

    added button styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; 
 import logo from '../svg/logo.svg';
-import { NavLink } from 'react-router-dom';  
+import { NavLink, Link } from 'react-router-dom';  
 import styled from 'styled-components'; 
 
 class Header extends Component {
@@ -15,7 +15,7 @@ class Header extends Component {
         <div className='header-conent'>
           <h1>See what's next</h1>
           <SubTitle>WATCH ANYWHERE. CANCEL ANYTIME</SubTitle>
-          <button>try it now</button>
+          <Link className='main-offer-btn'>try it now</Link>
         </div>
       </HeaderComponent>
     );
@@ -59,7 +59,41 @@ const HeaderComponent = styled.div`
 .header-top {
   position: relative; 
   height: 10rem; 
+  z-index: 1; 
   }
+
+
+// Header Content
+.header-content {
+  .header-content {
+    width: 65%; 
+    position: relative; 
+    margin: 4.5rem auto 0; 
+    display: flex; 
+    justify-content: center; 
+    align-center: center; 
+    text-align: center; 
+    flex-direction: column; 
+    z-index: 1; 
+  }
+}
+
+// Main Offer Btn
+.main-offer-btn {
+  display: inline-block; 
+  background: var(--main-red); 
+  text-transform: uppercase; 
+  border: none; 
+  outline: none; 
+  margin: 0 33%; 
+  padding: 1.5rem; 
+  border-radius: 0.1875rem; 
+  font-size: 2rem; 
+  text-align: center; 
+  box-shadow: 0 1px 0 rgba(0, 0, 0.45); 
+  transition: background 0.2s ease-in; 
+  cursor: pointer; 
+}
 `;
 
 // Main Title
