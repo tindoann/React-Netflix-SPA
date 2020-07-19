bd0fd8880bb00376994fd3647bc59df64c583352
tindoann <tindoann@gamil.com>
2020-07-19 16:07:32 -0400

    finish tab section

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -18,9 +18,9 @@ class TabComponent extends Component {
           onSelect={tabIndex => this.setState({ tabIndex })}
         >
           <TabList className='tab-nav-container'>
-            <Tab className={`${this.state.tabIndex === 0 ? 'active' : null}`}>
+            <Tab className={`${this.state.tabIndex === 0 ? 'tab-selected active' : null}`}>
               <TabDoor/>
-              <p>
+              <p style={{ marginBottom: "1.875rem" }}>
                 <strong>
                   No commitments
                   <br />
@@ -28,13 +28,13 @@ class TabComponent extends Component {
                 </strong>
               </p>
             </Tab>
-            <Tab className={`${this.state.tabIndex === 1 ? 'active' : null}`}>
+            <Tab className={`${this.state.tabIndex === 1 ? 'tab-selected active' : null}`}>
               <TabDevices/>
               <p style={{ marginTop: '-5.3125rem' }}>
                 <strong>Watch anywhere</strong>
               </p>
             </Tab>
-            <Tab className={`${this.state.tabIndex === 2 ? 'active' : null}`}>
+            <Tab className={`${this.state.tabIndex === 2 ? 'tab-selected active' : null}`}>
               <TabPrices/>
               <p>
                 <strong>Pick your price</strong>
