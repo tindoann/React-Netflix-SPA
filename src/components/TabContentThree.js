e141893e48546d5d2e369b1b2314ef9ad00d415d
tindoann <tindoann@gamil.com>
2020-07-20 23:31:01 -0400

    finish tab3 styling

--- src/components/TabContentThree.js
+++ src/components/TabContentThree.js
@@ -134,4 +134,41 @@ const TabContainer = styled.div`
     margin-left: 3rem; 
     margin-right: 5.1rem; 
   }
+
+  // Tab Bottom Content
+  .tab-bottom-content {
+    margin: 2rem auto; 
+  }
+
+  // Table
+  table {
+    width: 100%; 
+    margin-top: 2rem; 
+    border-collapse: collapse; 
+  }
+
+  table thread th {
+    text-transform: uppercase; 
+    padding: 0.8rem; 
+  }
+
+  table tbody {
+    display: table-row-group; 
+    vertical-align: middle; 
+    border-color: inherit; 
+  }
+
+  table tbody tr td {
+    color: #999; 
+    padding: 0.8rem 1.2rem; 
+    text-align: center; 
+  }
+
+  table tbody tr td:first-child {
+    text-align: left; 
+  }
+
+  table tbody tr:nth-child(even) {
+    background: #222; 
+  }
 `;
